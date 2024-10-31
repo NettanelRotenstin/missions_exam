9c9f3869d6e5481d8ad958fc7f4a5901e3a25bd5
Netanel Rotenstein <email@example.com>
2024-10-31 09:35:30 +0200

    class Mission

--- src/Models/Mission.ts
+++ src/Models/Mission.ts
@@ -1,11 +1,13 @@
 import {v4} from 'uuid'
 import StatusEnum from './StatusEnum'
+import PriorityEnum from './PriorityEnum'
 export default class Mission {
     public id:string
     constructor(
         public name:string,
         public completed:  StatusEnum,
-        priority:     
+        priority:PriorityEnum,
+        description:string
     ) {
         this.id = v4()
     }
