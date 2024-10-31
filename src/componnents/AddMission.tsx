16b979e40f99cec6fbe79f5e6570dbba9fe6b8f4
Netanel Rotenstein <email@example.com>
2024-10-31 09:43:03 +0200

    structure of adding mission

--- src/componnents/AddMission.tsx
+++ src/componnents/AddMission.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import StatusEnum from '../Models/StatusEnum'
+import PriorityEnum from '../Models/PriorityEnum'
+
 
 export default function AddMission() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <>
+            <input type="text" placeholder='type name of soldier' />
+            <select name="stat" id="">
+                <option value=''>{StatusEnum[0]}</option>
+                <option value=''>{StatusEnum[1]}</option>
+                <option value=''>{StatusEnum[2]}</option>
+            </select>
+            <select name="prior" id="">
+                <option value=''>{PriorityEnum[0]}</option>
+                <option value=''>{PriorityEnum[1]}</option>
+                <option value=''>{PriorityEnum[2]}</option>
+            </select>
+            <input type="text" placeholder='description'/>
+            <button onClick={()=>{}}>Add Mission</button>
+        </>
+    )
 }
